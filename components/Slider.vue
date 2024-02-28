1d6d0bb29df8857fa5ba8010b0f1144cd23c0b9f
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2024-02-29 00:58:59 +0200

    initial

---
+++ components/Slider.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+  import { Navigation, Pagination } from "swiper/modules";
+  import { Swiper, SwiperSlide } from "swiper/vue";
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  const prev = ref(null);
+  const next = ref(null);
+  const slide = ref(0);
+  const images = [
+    'https://picsum.photos/200/200',
+    'https://picsum.photos/300/200',
+    'https://picsum.photos/400/200'
+  ];
+
+  // const glob = import.meta.glob('../img/cars/0/img-slider/*.png', { eager: true });
+</script>
+
+<template>
+  <!--{{ glob }}-->
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <swiper :modules="[Navigation, Pagination]" :navigation="true" :pagination="{ clickable: true }">
+          <swiper-slide v-for="(img, id) in images" :key="id">
+            <v-sheet class="d-flex justify-center w-100">
+              <v-img :src="img" height="565" width="auto"></v-img>
+            </v-sheet>
+          </swiper-slide>
+        </swiper>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+  .swiper-button-prev {
+    width: 60px;
+    height: 60px;
+    margin-top: -30px;
+    left: 0;
+
+    &:after {
+      content: none;
+    }
+
+    @media (max-width: 600px) {
+      width: 30px;
+      height: 30px;
+      margin-top: -15px;
+    }
+  }
+
+  .swiper-button-next {
+    width: 60px;
+    height: 60px;
+    margin-top: -30px;
+    right: 0;
+
+    &:after {
+      content: none;
+    }
+
+    @media (max-width: 600px) {
+      width: 30px;
+      height: 30px;
+      margin-top: -15px;
+    }
+  }
+</style>
--- pages/cars/[id].vue
+++ pages/cars/[id].vue
@@ -1,74 +1,14 @@
-<script lang="ts" setup>
-  import { useDisplay } from "vuetify";
-  import { useCarsStore } from "~/stores/cars";
+<script setup lang="ts">
 
-  const cars = useCarsStore();
-  const display = ref(useDisplay());
-  const { xs } = useDisplay();
-
-  const route = useRoute();
-  const id = ref(route.params?.id);
-  const car = computed(() => cars.getCarById(+id.value));
-
-  const menuItems = [
-    "Головна",
-    "Опис",
-    "Збірка",
-    "Контакти"
-  ];
+import Header from "~/components/Header.vue";
+import Swiper from "~/components/Slider.vue";
 </script>
 
 <template>
-  <v-container class="pr-0 container-1440" fluid>
-    <v-row>
-
-      <!--LEFT-->
-      <v-col cols="12" md="6" order="2" order-md="1">
-        <v-card :class="{'column-right container-50': display.mdAndUp}" variant="text" class="h-100">
-          <v-row class="h-100">
-            <v-spacer/>
-            <v-col cols="11" class="d-flex align-center justify-center">
-              <v-sheet max-width="413" class="text-black">
-                <h3 class="fz-36 font-weight-regular mb-8">Конструктор</h3>
-                <h2 class="fz-68 font-weight-regular title lh-1 mb-1">{{ car.name }}</h2>
-                <span class="fz-36 font-weight-regular lh-1 title mb-1">Technic</span>
-                <p class="fz-36 title text-mainRed mb-8 lh-1">{{ car.price }} грн</p>
-                <p class="fz-20 lh-normal font-weight-regular mb-12">{{ car.subtitle }}</p>
-                <v-btn class="bg-red ls-normal text-none fz-20 font-weight-medium" size="large" width="200">Придбати</v-btn>
-              </v-sheet>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-
-      <!--RIGHT-->
-      <v-col class="rounded-s-xl" cols="12" md="6" order="1" order-md="2">
-        <v-card class="bg-blue h-100" variant="text">
-          <v-img :src="`/img/cars/${id}/car-header.png`" class="w-100">
-            <v-container :class="{'column-left container-50': display.mdAndUp}" class="h-100">
-              <v-row class="justify-end mx-n5" no-gutters>
-                <v-col v-for="(item, key) in menuItems" :key="key" class="mx-5" cols="auto">
-                  <span class="title fs-18 text-white font-weight-regular">{{ item }}</span>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-img>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <Header/>
+  <Swiper/>
 </template>
 
 <style scoped>
-  .container-50 {
-    max-width: 530px;
-  }
 
-  .column-left {
-    margin: auto 0 0 0;
-  }
-
-  .column-right {
-    margin: 0 0 0 auto;
-  }
 </style>
