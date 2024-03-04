b9fccbab6c8f07f8611048ec85cc359e0a8ddf93
Max-Lutskov <makslutskov1@gmail.com>
2024-03-04 15:27:03 +0200

    щось зробив

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Thanks Page</h1>
+  <v-container class="h-100">
+    <img height="1" width="1" style="display:none" alt="" src="https://www.facebook.com/tr?id=285706427331877&ev=Lead&noscript=1" />
+    <v-row class="h-100" align="center" justify="center">
+      <v-col cols="12" md="8">
+        <fieldset class="card-border rounded-xl">
+        <v-card height="100%" variant="text">
+          <v-card-text class="text-center py-15">
+            <h2 class="mb-4">Дякуємо за замовлення!</h2>
+            <p class="mb-4">Найближчим часом ми з вами зв'яжемось.</p>
+            <nuxt-link class="link border px-4  py-2 d-inline-block text-black"  to="/0">Back to Site</nuxt-link>
+          </v-card-text>
+        </v-card>
+        </fieldset>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<style>
+
+  .card-border {
+    border-bottom: 12px solid red;
+    border-top: 12px solid red;
+    border-right: 12px solid red;
+    border-left:  12px solid red;
+
+    @media(max-width: 600px) {
+      border: none;
+    }
+  }
+</style>
