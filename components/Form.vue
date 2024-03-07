d888bd44625ca0416465c958c37f58e3a0cdff24
Max-Lutskov <makslutskov1@gmail.com>
2024-03-07 23:13:04 +0200

    щось зробив

--- components/Form.vue
+++ components/Form.vue
@@ -43,8 +43,8 @@
               <v-sheet :max-width="display.smAndDown ? 'unset' : 518" class="text-black" width="100%">
                 <h5 class="fz-32m font-weight-regular lh-1 mb-4">Бажаєте замовити?</h5>
                 <p class="fz-20 font-weight-regular lh-140 mb-6">Залиште нам коментар, або зв’яжіться з нами через Instagram або Telegram</p>
-                <v-form v-model="valid" data-netlify="true" action="/thanks" data-netlify-honeypot="bot-field" method="POST" name="contact">
-                  <input name="form-name" type="hidden" value="contact"/> <input name="bot-field" type="hidden"/>
+                <v-form v-model="valid" data-netlify="true" action="/thanks" data-netlify-honeypot="bot-field" method="POST" name="contactShop">
+                  <input name="form-name" type="hidden" value="contactShop"/> <input name="bot-field" type="hidden"/>
                   <v-text-field v-model="data.name" :rules="[v => !!v || 'Це поле необхідно заповнити']" class="mb-1" label="Ім’я" name="name" type="text" variant="outlined"></v-text-field>
                   <v-text-field v-model="data.phone" :rules="[v => !!v || 'Це поле необхідно заповнити']" class="mb-1" label="Телефон" name="phone" type="text" variant="outlined"></v-text-field>
                   <v-textarea v-model="data.message"  label="Залиште коментар" name="message" variant="outlined"></v-textarea>
