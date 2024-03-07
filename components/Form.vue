695240b2cecc9149d9e940ec6d1c1a6f903d9120
Max-Lutskov <makslutskov1@gmail.com>
2024-03-08 00:19:17 +0200

    щось зробив

--- components/Form.vue
+++ components/Form.vue
@@ -43,7 +43,7 @@
               <v-sheet :max-width="display.smAndDown ? 'unset' : 518" class="text-black" width="100%">
                 <h5 class="fz-32m font-weight-regular lh-1 mb-4">Бажаєте замовити?</h5>
                 <p class="fz-20 font-weight-regular lh-140 mb-6">Залиште нам коментар, або зв’яжіться з нами через Instagram або Telegram</p>
-                <v-form v-model="valid" name="contact" method="POST" data-netlify="true" @submit="sendMessage" action="/thanks">
+                <v-form v-model="valid" name="contact" method="POST" netlify @submit="sendMessage" action="/thanks">
                   <input type="hidden" name="form-name" value="contact">
                   <v-text-field v-model="data.name" :rules="[v => !!v || 'Це поле необхідно заповнити']" class="mb-1" label="Ім’я" name="name" type="text" variant="outlined"></v-text-field>
                   <v-text-field v-model="data.phone" :rules="[v => !!v || 'Це поле необхідно заповнити']" class="mb-1" label="Телефон" name="phone" type="text" variant="outlined"></v-text-field>
