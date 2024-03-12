6cbf40f1ec515b0bf87f997b9da1053547bb9823
Vladyslav Lukiianchuk <vladlukiyanchukoo@gmail.com>
2024-03-12 16:21:40 +0200

    catalog

--- components/Header.vue
+++ components/Header.vue
@@ -13,10 +13,9 @@
   const car = computed(() => cars.getCarById(+id.value));
 
   const rewardFilters = reactive([
-    { name: "Головна", link: "#header" },
-    { name: "Опис", link: "#desc" },
-    { name: "Збірка", link: "#functions" },
-    { name: "Контакти", link: "#form" },
+    { name: "Опис", link: "#desc", show: true },
+    { name: "Збірка", link: "#functions", show: car.value.functions },
+    { name: "Контакти", link: "#form", show: true },
   ]);
 
   const selectedFilter = ref();
@@ -26,7 +25,7 @@
     if (link != -1) {
       selectedFilter.value = link;
     } else {
-      selectedFilter.value = 0;
+      selectedFilter.value = -1;
     }
   });
 
@@ -71,9 +70,12 @@
       <!--LEFT-->
       <v-col class="px-7" cols="12" md="6" order="2" order-md="1">
         <v-card :class="{'column-right container-50': display.mdAndUp}" class="h-100" variant="text">
+          <v-btn v-if="display.mdAndUp" icon="" size="x-large" to="/" variant="text">
+            <Icon name="mdi:arrow-left-thick" size="60"></Icon>
+          </v-btn>
           <v-row class="h-100">
             <v-col class="d-flex align-center">
-              <v-sheet :max-width="xs ? 'unset' : 413" class="text-black">
+              <v-sheet :max-width="display.smAndDown ? 'unset' : 413" class="text-black">
                 <h3 class="fz-36 font-weight-regular mb-8">Конструктор</h3>
                 <h2 class="fz-68 font-weight-regular title lh-1 mb-1">{{ car.name }}</h2>
                 <span class="fz-36 font-weight-regular lh-1 title mb-1">Technic</span>
@@ -91,16 +93,21 @@
         <v-card class="h-100 rounded-0 mx-n3 mx-md-0" variant="text">
           <v-img :src="getOnePicture('car-header')" class="w-100">
             <div class="menu-gradient">
-              <v-container :class="{'column-left container-50': display.mdAndUp}" class="h-100 pr-7">
+              <v-container :class="{'column-left container-50': display.mdAndUp}" class="h-100 pr-7 d-flex d-md-block align-center justify-space-between">
+                <v-btn v-if="display.smAndDown" icon="" size="small" to="/" variant="text">
+                  <Icon color="white" name="mdi:arrow-left-thick" size="35"></Icon>
+                </v-btn>
                 <v-item-group v-model="selectedFilter" mandatory>
                   <v-row class="mx-sm-n5 justify-end" no-gutters>
-                    <v-col v-for="(filter, index) in rewardFilters" :key="index" class="fz-20 mx-3 mx-sm-5" cols="auto">
-                      <v-item v-slot="{ isSelected, toggle }" :value="filter">
-                        <nuxt-link :class="isSelected ? 'text-mainRed font-weight-bold' : 'text-white custom-link'" :href="`${filter.link}`" class="link cursor-pointer font-weight-medium fz-18" @click="scrollToAnchor(filter.link)">
-                          <span @click="toggle">{{ filter.name }}</span>
-                        </nuxt-link>
-                      </v-item>
-                    </v-col>
+                    <template v-for="(filter, index) in rewardFilters" :key="index">
+                      <v-col v-if="filter.show" class="fz-20 mx-3 mx-sm-5" cols="auto">
+                        <v-item v-slot="{ isSelected, toggle }" :value="filter">
+                          <nuxt-link :class="isSelected ? 'text-mainRed font-weight-bold' : 'text-white custom-link'" :href="`${filter.link}`" class="link cursor-pointer font-weight-medium fz-18" @click="scrollToAnchor(filter.link)">
+                            <span @click="toggle">{{ filter.name }}</span>
+                          </nuxt-link>
+                        </v-item>
+                      </v-col>
+                    </template>
                   </v-row>
                 </v-item-group>
               </v-container>
